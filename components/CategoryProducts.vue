00c94efc528c858949813ac87eb1e4c3c406fb61
menezescosta <mc611641@gmail.com>
2023-04-18 20:20:16 +0100

    feat: adicionando o componente singleProduct

--- components/CategoryProducts.vue
+++ components/CategoryProducts.vue
@@ -1,17 +1,48 @@
 <script setup>
 import { defineProps } from "vue";
-defineProps(["title", "products", "tabs"]);
+    defineProps(["title", "products", "tabs"]);
 </script>
 
 <template>
     <section class="category-products">
         <div class="container">
             <CategoryHeader :title="title" :tabs="tabs"/>
+            <div class="products">
+                <SingleProduct v-for="(product, i) in products" 
+                    :key="i"
+                    :product="product"
+                />
+            </div>
+            <button class="btn">SEE ALL</button>
         </div>
     </section>
 </template>
 
-<style>
-
+<style scoped>
+.catagory-products {
+  margin-bottom: 100px;
+}
+.catagory-products .btn {
+  height: 48px;
+  background: #f8f8f8;
+  border: 2px solid #f8f8f8;
+  max-width: 100%;
+  margin-top: 18px;
+}
+.products {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  gap: 30px;
+}
+@media (max-width: 969px) {
+  .products {
+    grid-template-columns: 1fr 1fr;
+  }
+}
+@media (max-width: 400px) {
+  .products {
+    grid-template-columns: 1fr;
+  }
+}
 </style>
 
---
+++ components/SingleProduct.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="single-product">
+        <div class="img-container">
+            <img :src="getImage(product.img)" alt="">
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+defineProps({
+  product: {
+    type: Object,
+  },
+});
+// new URL('./logo.png', import.meta.url).href
+const getImage = (src) => {
+  return new URL(src, import.meta.url).href;
+};
+</script>
+
+<style scoped>
+.single-product .img-container {
+  position: relative;
+}
+.single-product .img-container .sale {
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 9px;
+  background: #e91e63;
+  right: auto;
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 13px;
+  text-align: center;
+
+  /* white */
+
+  color: #ffffff;
+}
+.single-product .product-content {
+  padding: 10px 0px;
+}
+.single-product .product-content h6 {
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 16px;
+  opacity: 0.6;
+}
+.single-product .product-content .rating {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 5px;
+}
+.single-product .rating .price {
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 16px;
+}
+</style>
